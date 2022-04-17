4eb3b3eca7217eda81782207f69673631e34772b
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2022-04-17 19:28:44 -0400

    node-service removed

--- code-deploy/applicationStart.sh
+++ code-deploy/applicationStart.sh
@@ -7,4 +7,3 @@ sudo pm2 startup systemd
 sudo pm2 save
 sudo ln -sf /home/ec2-user/node_app/node-service.service /etc/systemd/system/node-service.service
 sudo systemctl daemon-reload
-sudo systemctl restart node-service.service
